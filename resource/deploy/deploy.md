8ec39af329292849c05b602647333acd0f3a6b68
gho9o9 <o9o9@pg7.so-net.ne.jp>
2022-01-24 22:55:19 +0900

    commit

---
+++ resource/deploy/deploy.md
@@ -0,0 +1,1 @@
+[![Deploy to Azure](https://aka.ms/deploytoazurebutton)](https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Fgho9o9%2Fshare%2Fmain%2Ftest%2FazureDeploy.json)
---
+++ resource/script/Dedicated_DDL.sql
@@ -0,0 +1,35 @@
+-- 5章、6章
+create table [adpe2e].[TaxiDataSummary]
+(
+    [PickUpDate] [date] NULL,
+    [PickUpBorough] [varchar](200) NULL,
+    [PickUpZone] [varchar](200) NULL,
+    [PaymentType] [varchar](11) NULL,
+    [TotalTripCount] [int] NULL,
+    [TotalPassengerCount] [int] NULL,
+    [TotalDistanceTravelled] [decimal](38, 2) NULL,
+    [TotalTipAmount] [decimal](38, 2) NULL,
+    [TotalFareAmount] [decimal](38, 2) NULL,
+    [TotalTripAmount] [decimal](38, 2) NULL
+)
+with
+(
+    distribution = round_robin,
+    clustered columnstore index
+)
+
+go
+
+create table [adpe2e].[TaxiLocationLookup]
+(
+    [LocationID] [int] NULL,
+    [Borough] [varchar](200) NULL,
+    [Zone] [varchar](200) NULL,
+    [ServiceZone] [varchar](200) NULL
+)
+with
+(
+    distribution = round_robin,
+    clustered columnstore index
+)
+go
