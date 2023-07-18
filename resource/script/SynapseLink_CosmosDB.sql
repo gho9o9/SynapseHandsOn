87a36b6b9c9ce45836aa0ea63b0813cd2e146e9f
gho9o9 <o9o9@pg7.so-net.ne.jp>
2023-07-19 08:33:17 +0900

    commit

--- resource/script/SynapseLink_CosmosDB.sql
+++ resource/script/SynapseLink_CosmosDB.sql
@@ -6,14 +6,61 @@ CREATE CREDENTIAL MyCosmosDbAccountCredential
 WITH IDENTITY = 'SHARED ACCESS SIGNATURE', SECRET = '<Azure Cosmos DB キー>'
 GO
 
-SELECT Country, City, Total_Customers, Total_Orders, Total_Revenue, Rank_Revenue, *
+SELECT top 5 *
 FROM OPENROWSET(
       PROVIDER = 'CosmosDB',
       CONNECTION = 'Account=<Azure Cosmos DB アカウント名>;Database=SalesDB',
-      OBJECT = 'Sales',
+      OBJECT = 'SalesOrder',
       SERVER_CREDENTIAL = 'MyCosmosDbAccountCredential'
     ) AS rows
 GO
+
+SELECT top 5 *
+FROM OPENROWSET(
+      PROVIDER = 'CosmosDB',
+      CONNECTION = 'Account=<Azure Cosmos DB アカウント名>;Database=SalesDB',
+      OBJECT = 'Customer',
+      SERVER_CREDENTIAL = 'MyCosmosDbAccountCredential'
+    ) AS rows
+GO
+
+CREATE VIEW SalesOrderView AS
+SELECT *
+FROM OPENROWSET(
+      PROVIDER = 'CosmosDB',
+      CONNECTION = 'Account=<Azure Cosmos DB アカウント名>;Database=SalesDB',
+      OBJECT = 'SalesOrder',
+      SERVER_CREDENTIAL = 'MyCosmosDbAccountCredential'
+    ) AS rows
+GO
+
+CREATE VIEW CustomerView AS
+SELECT *
+FROM OPENROWSET(
+      PROVIDER = 'CosmosDB',
+      CONNECTION = 'Account=<Azure Cosmos DB アカウント名>;Database=SalesDB',
+      OBJECT = 'Customer',
+      SERVER_CREDENTIAL = 'MyCosmosDbAccountCredential'
+    ) AS rows
+GO
+
+SELECT 
+  s.id as SalesOrderId
+  , c.id AS CustomerId
+  , JSON_VALUE(c.addresses, '$[0].country') AS Country
+  , JSON_VALUE(c.addresses, '$[0].city')  AS City
+  , s.orderdate AS OrderDate
+  , s.shipdate AS ShipDate
+FROM
+  CustomerView c 
+INNER JOIN
+  SalesOrderView s
+ON c.id = s.CustomerId
+GO
+
+DROP VIEW CustomerView
+DROP VIEW SalesOrderView
+
 
 DROP CREDENTIAL MyCosmosDbAccountCredential
 GO
